453cfd6b59ad8636e5c8715539bec40f76db1abe
suman sourabh satpathy <46081174+suman104@users.noreply.github.com>
2019-05-05 03:47:47 +0530

    latest

--- T2RCIT/backend/T2RCIT/src/main/java/com/tcs/T2RCIT/controller/MyController.java
+++ T2RCIT/backend/T2RCIT/src/main/java/com/tcs/T2RCIT/controller/MyController.java
@@ -1,7 +1,10 @@
 package com.tcs.T2RCIT.controller;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins="http://localhost:4200")
@@ -10,11 +13,12 @@ public class MyController {
 	@GetMapping(path="/appName")
 	public String getAppName() {
 		return "T2RCIT";
+		//throw new RuntimeException("error has happened");
 	}
 	
-	@PostMapping(path="/login")
-	public String login() {
-		return "Success";
+	@GetMapping(path="/login/{username}")
+	public String login(@PathVariable String username) {
+		return "Hello "+username;
 	}
 	
 	
