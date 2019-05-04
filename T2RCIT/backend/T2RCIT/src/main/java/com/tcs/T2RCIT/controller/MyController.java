955dee3fbd8b392239694f6bc37d99b9d1304be0
suman sourabh satpathy <46081174+suman104@users.noreply.github.com>
2019-05-05 01:31:12 +0530

    updated

--- T2RCIT/backend/T2RCIT/src/main/java/com/tcs/T2RCIT/controller/MyController.java
+++ T2RCIT/backend/T2RCIT/src/main/java/com/tcs/T2RCIT/controller/MyController.java
@@ -1,13 +1,21 @@
 package com.tcs.T2RCIT.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class MyController {
 	@GetMapping(path="/appName")
 	public String getAppName() {
 		return "T2RCIT";
 	}
+	
+	@PostMapping(path="/login")
+	public String login() {
+		return "Success";
+	}
+	
+	
 }
